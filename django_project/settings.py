7cfc209e3b1ff4a0dd37a6c9f5eb2be629373ea7
Niyazahamad786 <niyazahamad195@gmail.com>
2020-12-03 13:16:00 -0800

    staticfile creation

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,12 +129,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL='/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,  './blog/static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
